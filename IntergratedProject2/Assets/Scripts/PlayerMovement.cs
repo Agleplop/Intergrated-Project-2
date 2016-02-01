3826226efc4f2964ae1d6ede300c79278be9e8fd
Agleplop <arduncan111@gmail.com>
2016-02-01 22:14:32 +0000

    Project Creation
    Basic jump implemented. Double jump and better movement next.

---
+++ IntergratedProject2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementSpeed;
+	public float jumpHeight;
+	bool grounded;
+	bool hasJumped;
+	bool doubleJump;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		grounded = false;
+		hasJumped = false;
+		doubleJump = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+		}
+
+		if(!grounded && rigidbody2D.velocity.y == 0) 
+		{
+			grounded = true;
+		}
+		
+		if (Input.GetKey(KeyCode.UpArrow) && (grounded || doubleJump)) 
+		{
+			if (doubleJump = false)
+				doubleJump = true;
+			else
+				doubleJump = false;
+			print (doubleJump);
+			
+			hasJumped = true;
+		}
+
+
+	}
+
+	void FixedUpdate ()
+	{
+		if(hasJumped)
+		{
+			rigidbody2D.AddForce(transform.up*jumpHeight);
+			grounded = false;
+			hasJumped = false;
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
